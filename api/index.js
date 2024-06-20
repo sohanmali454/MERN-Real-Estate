8dc8b1e015853d7d8b037a83b0c4be7ee736ff1b
sohanmali454 <malisohan454@gmail.com>
2024-06-20 23:45:30 +0530

    create a test API routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO)
     });
 
 const app = express();
+
+app.use("/crystalEstateApp", userRoutes)
 
 app.listen(3000, () => {
     console.log('Server is listining on port 3000!!!');
