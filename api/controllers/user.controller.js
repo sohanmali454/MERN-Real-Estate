15b1eaeb5a8ccabb5be1cfef42cf5f548084f097
sohanmali454 <malisohan454@gmail.com>
2024-06-30 23:05:01 +0530

    UserUpdate-1

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,7 @@
 export const test = (req, res) => {
     res.json({ message: 'API is Working' });
 }
+
+export const updateUser = (req, res, next) => {
+
+}
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(MONGO_URI).then(() => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}!!!`);
