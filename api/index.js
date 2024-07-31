dfcb07b74ce189eac7c92f0237ca5ac87913adc4
sohanmali454 <malisohan454@gmail.com>
2024-07-31 23:33:16 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import listingRoutes from './routes/listing.route.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
 dotenv.config();
@@ -12,32 +13,33 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO;
 
 mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err.message);
-  });
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err.message);
+    });
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}!!!`);
+    console.log(`Server is listening on port ${PORT}!!!`);
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/listing", listingRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
 
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
